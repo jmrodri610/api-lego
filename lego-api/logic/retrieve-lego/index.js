5727515db040754206a3727dd22b77b67eeef07d
Jose Rodriguez <jechto610@gmail.com>
2020-01-30 11:50:04 +0100

    update retrieve-lego testing, init utils module and errors

--- lego-api/logic/retrieve-lego/index.js
+++ lego-api/logic/retrieve-lego/index.js
@@ -1,6 +1,12 @@
 const legos = require('../../../data')
+const { errors: {ContentError} } = require('utils')
+
 
 module.exports = function (id) {
+    
+    if (typeof id !== 'string') throw new TypeError (`${id} is not a string`)
+
+
     return (async () => {
 
         const lego = await legos.find(lego => lego.id === id)
--- lego-api/logic/retrieve-lego/index.spec.js
+++ lego-api/logic/retrieve-lego/index.spec.js
@@ -1,5 +1,6 @@
 const {expect} = require('chai')
 const retrieveLego = require('.')
+const { errors: { TypeError } } = require('utils')
 
 describe('retrieve lego', () => {
     it('should return a unique lego based on an id', async () => {
@@ -26,5 +27,15 @@ describe('retrieve lego', () => {
            expect(error).to.exist
            expect(error.message).to.equal('lego not found') 
         }
+    })
+
+    it('should fail on an incorrect data type on id', ()=> {
+
+        expect(() => retrieveLego(1)).to.throw(TypeError, '1 is not a string')
+        expect(() => retrieveLego(true)).to.throw(TypeError, 'true is not a string')
+        expect(() => retrieveLego({})).to.throw(TypeError, '[object Object] is not a string')
+        expect(() => retrieveLego(undefined)).to.throw(TypeError, 'undefined is not a string')
+        expect(() => retrieveLego(null)).to.throw(TypeError, 'null is not a string')
+
     })
 })
